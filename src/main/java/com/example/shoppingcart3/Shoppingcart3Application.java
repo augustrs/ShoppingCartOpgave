8cc910ba9362109e3c3cdcc89d31b0e14a105503
augustrs <auso0001@stud.kea.dk>
2024-03-20 19:38:11 +0100

    Initial commit

---
+++ src/main/java/com/example/shoppingcart3/Shoppingcart3Application.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcart3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shoppingcart3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Shoppingcart3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shoppingcart3/Shoppingcart3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingcart3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shoppingcart3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
